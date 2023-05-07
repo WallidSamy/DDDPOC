bd88633c53a62158bb3f45d3414dad7badf7e6c0
walid.samy@assetdev.com <walid.samy@assetdev.com>
2023-05-07 16:15:38 +0300

    add projects

---
+++ DDDPOC.Application/Commands/Customer/AddCustomerCommand.cs
@@ -0,0 +1,47 @@
+﻿using DDDPOC.Infrastructure.Repositories;
+using MediatR;
+using System.ComponentModel.DataAnnotations;
+using DDDPOC.Domain.Aggregates;
+
+
+namespace DDDPOC.Application.Commands
+{
+    public class AddCustomerCommand : IRequest<bool>
+    {
+        [Required]
+        public string CustomerName { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public string Email { get; set; }
+    }
+
+    public class AddCustomerHandler : IRequestHandler<AddCustomerCommand, bool>
+    {
+        private readonly IRepository<Customer, Guid> _customerRepo;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public AddCustomerHandler(IRepository<Customer, Guid> customerRepo,
+                                IUnitOfWork unitOfWork)
+        {
+            _customerRepo = customerRepo;
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<bool> Handle(AddCustomerCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var customer = Customer.Create(request.CustomerName, request.Address, request.Email);
+                _customerRepo.Add(customer);
+                _unitOfWork.SaveChanges();
+                await _customerRepo.RaisEvents(customer);
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+                return false;
+            }
+        }
+    }
+}
--- DDDPOC.UI/Program.cs
+++ DDDPOC.UI/Program.cs
@@ -1,12 +1,20 @@
+using DDDPOC.Infrastructure.Infrastructure.Data;
+using DDDPOC.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(AppDomain.CurrentDomain.Load("DDDPOC.Application")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddScoped(typeof(IRepository<,>), typeof(Repository<,>));
+builder.Services.AddScoped(typeof(IUnitOfWork), typeof(UnitOfWork));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
